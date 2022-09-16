db780a00930f3e264e7a3659bd9410e8bec456b3
Jose Fernando Mendes da Costa <98fernandocosta@gmail.com>
2022-09-16 20:06:30 -0300

    seed da startup

--- database/seeders/StartupSeeder.php
+++ database/seeders/StartupSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Startup;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class StartupSeeder extends Seeder
@@ -13,6 +15,6 @@ class StartupSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Startup::factory()->withLogo()->hasEndereco()->hasTelefones(1)->hasDocumentos(3)->for(User::where('tipo', 1)->first())->forArea()->count(50)->create();
     }
 }
