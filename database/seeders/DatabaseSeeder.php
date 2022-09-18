b65187fe1379a74903a9409b75e255ecd8309c92
Jose Fernando Mendes da Costa <98fernandocosta@gmail.com>
2022-09-18 19:54:21 -0300

    seeder de plano

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             AreaSeeder::class,
             UserSeeder::class,
             StartupSeeder::class,
+            PlanoSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/PlanoSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Plano;
+use Illuminate\Database\Seeder;
+
+class PlanoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Plano::factory()->count(5)->create();
+    }
+}
